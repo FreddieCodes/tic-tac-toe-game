65de9e395cd1b16cb5e5f7f7300a619bf5571dee
FreddieCodes <FrederickCodes@gmail.com>
2023-04-11 11:02:45 +0100

    infobar added to Game Board

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -2,6 +2,23 @@ import React from 'react';
 
 export default function GameBoard(props) {
   return (
-    <div>{props.gameType}</div>
+    <div className="game-board-cntnr">
+      <div className="game-board">
+        <div className="infobar">
+          <div className="gb-logo-cntnr">
+            <img src="/img/logo.svg" alt="logo" className="logo"/>
+          </div>
+          <div className="turn-indicator">
+            <img src="/img/turn-o-grey-small.svg" alt="X" className=""/>
+            <h2>TURN</h2>
+          </div>
+          <div className="restart-btn">
+            <img src="/img/restart-sign.svg" alt="restart" className="restart"/>
+          </div>
+        </div>
+        <div className="board"></div>
+        <div className="statbar"></div>
+      </div>
+    </div>
   )
 }
--- src/components/NewGameMenu.js
+++ src/components/NewGameMenu.js
@@ -6,27 +6,29 @@ export default function NewGameMenu({ handleGameStart, handleGameType }) {
     // if the class contains 'cpu' then call handleGameType with 'cpu' as the argument
     e.target.classList.contains("cpu") ? handleGameType('cpu') : handleGameType('player');
   }
-  
+
   return (
-    <div className="new-game-menu">
-        <div className="logo-cntnr">
-          <img src="/img/logo.svg" alt="logo" className="logo"/>
-        </div>
-        <div className="mark-cntnr">
-          <h2 className='player-name'>Pick Player 1's Mark</h2>
-          <div className="mark-select">
-              <button className='mark-select-btn x-side '>
-              <img src="/img/player_x.svg" alt="x button" className=""/>
-              </button>
-              <button className='mark-select-btn o-side player-choice'>
-              <img src="/img/player_o.svg" alt="o button" className=""/>
-              </button>
+    <div className="new-game-cntnr">
+      <div className="new-game-menu">
+          <div className="logo-cntnr">
+            <img src="/img/logo.svg" alt="logo" className="logo"/>
           </div>
-          <p>REMEMBER : X GOES FIRST</p>
-        </div>
-        <div className='game-type-cntnr'>
-          <button className="new-game-btn cpu" onClick={handleClick}>New Game (vs CPU)</button>
-          <button className="new-game-btn player" onClick={handleClick}>New Game (vs Player)</button>
+          <div className="mark-cntnr">
+            <h2 className='player-name'>Pick Player 1's Mark</h2>
+            <div className="mark-select">
+                <button className='mark-select-btn x-side '>
+                <img src="/img/player_x.svg" alt="x button" className=""/>
+                </button>
+                <button className='mark-select-btn o-side player-choice'>
+                <img src="/img/player_o.svg" alt="o button" className=""/>
+                </button>
+            </div>
+            <p>REMEMBER : X GOES FIRST</p>
+          </div>
+          <div className='game-type-cntnr'>
+            <button className="new-game-btn cpu" onClick={handleClick}>New Game (vs CPU)</button>
+            <button className="new-game-btn player" onClick={handleClick}>New Game (vs Player)</button>
+          </div>
         </div>
       </div>
   )
